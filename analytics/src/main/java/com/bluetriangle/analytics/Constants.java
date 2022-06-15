5277968d1991145e7c3f0a71702b738615be3c77
Jason Emerick <jemerick@gmail.com>
2022-06-15 15:46:29 -0400

    Begin moving constants to a separate class

---
+++ analytics/src/main/java/com/bluetriangle/analytics/Constants.java
@@ -0,0 +1,11 @@
+package com.bluetriangle.analytics;
+
+/**
+ * constant values used across the SDK
+ */
+final class Constants {
+    /**
+     * The hardcoded browser value
+     */
+    static final String BROWSER = "Native App";
+}
