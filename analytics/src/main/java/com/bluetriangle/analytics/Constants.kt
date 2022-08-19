155415dd3f0ec137a9ce8dade5da497f8c269a89
Jason Emerick <jemerick@gmail.com>
2022-08-19 00:10:53 -0400

    convert constants to kotlin

---
+++ analytics/src/main/java/com/bluetriangle/analytics/Constants.kt
@@ -0,0 +1,16 @@
+package com.bluetriangle.analytics
+
+/**
+ * constant values used across the SDK
+ */
+object Constants {
+    const val OS = "Android"
+    const val BROWSER = "Native App"
+    const val DEVICE_TABLET = "Tablet"
+    const val DEVICE_MOBILE = "Mobile"
+    const val UTF_8 = "UTF-8"
+    const val METHOD_POST = "POST"
+    const val HEADER_USER_AGENT = "User-Agent"
+    const val HEADER_CONTENT_TYPE = "Content-Type"
+    const val CONTENT_TYPE_JSON = "application/json; charset=utf-8"
+}
