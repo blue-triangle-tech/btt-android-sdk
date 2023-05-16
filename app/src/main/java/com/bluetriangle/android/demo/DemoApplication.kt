f73b8008c726c28f04cbef8d6529fd050e2f9c15
Ganesh <ganeshw@alohatechnology.com>
2023-05-16 16:16:22 +0530

    Demo app refactoring with gradle dependencies version update

---
+++ app/src/main/java/com/bluetriangle/android/demo/DemoApplication.kt
@@ -0,0 +1,23 @@
+package com.bluetriangle.android.demo
+
+import android.app.Application
+import com.bluetriangle.analytics.BlueTriangleConfiguration
+import com.bluetriangle.analytics.Tracker
+import com.bluetriangle.analytics.Tracker.Companion.init
+
+
+class DemoApplication : Application() {
+    private var tracker: Tracker? = null
+    override fun onCreate() {
+        super.onCreate()
+        val configuration = BlueTriangleConfiguration()
+        configuration.isTrackCrashesEnabled = true
+        configuration.siteId = "mobelux3271241z"
+        configuration.isDebug = true
+        configuration.networkSampleRate = 1.0
+        configuration.isPerformanceMonitorEnabled = true
+        tracker = init(applicationContext, configuration)
+
+        tracker!!.setSessionTrafficSegmentName("Demo Traffic Segment")
+    }
+}
