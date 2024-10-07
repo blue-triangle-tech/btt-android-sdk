a4c4e43d9678b5c7f1f5d6069d922f0be80c0696
Ismail <ismails@alohatechnologydev.com>
2024-10-07 13:20:36 +0530

    Removed Multidex setup

--- app/src/main/java/com/bluetriangle/android/demo/DemoApplication.kt
+++ app/src/main/java/com/bluetriangle/android/demo/DemoApplication.kt
@@ -1,11 +1,8 @@
 package com.bluetriangle.android.demo
 
 import android.app.Application
-import android.content.Context
 import android.util.Log
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.multidex.MultiDex
-import androidx.multidex.MultiDexApplication
 import com.bluetriangle.analytics.BlueTriangleConfiguration
 import com.bluetriangle.analytics.Tracker
 import com.bluetriangle.analytics.Tracker.Companion.init
@@ -15,7 +12,7 @@ import com.bluetriangle.android.demo.tests.ANRTestScenario
 import com.bluetriangle.android.demo.tests.HeavyLoopTest
 import com.bluetriangle.android.demo.tests.LaunchTestScenario
 
-class DemoApplication : MultiDexApplication() {
+class DemoApplication : Application() {
     private var tracker: Tracker? = null
 
     companion object {
