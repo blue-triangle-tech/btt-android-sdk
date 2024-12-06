24280531c9aef8b1a4d6692ac123578700ddf8fc
Ismail <ismails@alohatechnologydev.com>
2024-12-06 17:31:52 +0530

    Removed trust all certificates code

--- analytics/src/main/java/com/bluetriangle/analytics/dynamicconfig/fetcher/BTTConfigurationFetcher.kt
+++ analytics/src/main/java/com/bluetriangle/analytics/dynamicconfig/fetcher/BTTConfigurationFetcher.kt
@@ -16,12 +16,7 @@ import org.json.JSONObject
 import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URL
-import java.security.SecureRandom
-import java.security.cert.X509Certificate
 import javax.net.ssl.HttpsURLConnection
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
-import javax.net.ssl.X509TrustManager
 
 
 internal class BTTConfigurationFetcher(private val remoteConfigUrl: String) :
@@ -45,6 +40,7 @@ internal class BTTConfigurationFetcher(private val remoteConfigUrl: String) :
         } catch (e: IOException) {
             return BTTConfigFetchResult.Failure(BTTConfigFetchError.Other(e.message ?: ""))
         } catch (e: Exception) {
+            e.printStackTrace()
             return BTTConfigFetchResult.Failure(BTTConfigFetchError.Other(e.message ?: ""))
         }
     }
@@ -52,7 +48,6 @@ internal class BTTConfigurationFetcher(private val remoteConfigUrl: String) :
     private suspend fun URL.fetchJSON(): String = withContext(Dispatchers.IO) {
         var connection: HttpURLConnection? = null
         try {
-            trustAllCertificates()
             connection = (openConnection() as HttpsURLConnection).apply {
                 requestMethod = "GET"
                 connectTimeout = 5000
@@ -73,39 +68,6 @@ internal class BTTConfigurationFetcher(private val remoteConfigUrl: String) :
             }
         } finally {
             connection?.disconnect()
-        }
-    }
-
-    private fun trustAllCertificates() {
-        try {
-            val trustAllCerts = arrayOf<TrustManager>(
-                @SuppressLint("CustomX509TrustManager")
-                object : X509TrustManager {
-                    @SuppressLint("TrustAllX509TrustManager")
-                    override fun checkClientTrusted(
-                        chain: Array<X509Certificate>,
-                        authType: String
-                    ) {
-                    }
-
-                    @SuppressLint("TrustAllX509TrustManager")
-                    override fun checkServerTrusted(
-                        chain: Array<X509Certificate>,
-                        authType: String
-                    ) {
-                    }
-
-                    override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
-                }
-            )
-
-            val sslContext = SSLContext.getInstance("SSL")
-            sslContext.init(null, trustAllCerts, SecureRandom())
-            HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.socketFactory)
-
-            HttpsURLConnection.setDefaultHostnameVerifier { _, _ -> true }
-        } catch (e: Exception) {
-            e.printStackTrace()
         }
     }
 
