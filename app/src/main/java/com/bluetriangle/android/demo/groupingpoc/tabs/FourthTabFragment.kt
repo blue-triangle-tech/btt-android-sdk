1df1bd6455cffdb193b8297fa194f58cee59d706
Ismail <ismails@alohatechnologydev.com>
2025-07-09 18:52:54 +0530

    Updated version to 2.18.0-beta-1

--- app/src/main/java/com/bluetriangle/android/demo/groupingpoc/tabs/FourthTabFragment.kt
+++ app/src/main/java/com/bluetriangle/android/demo/groupingpoc/tabs/FourthTabFragment.kt
@@ -1,33 +1,15 @@
 package com.bluetriangle.android.demo.groupingpoc.tabs
 
 import android.os.Bundle
+import android.util.AndroidRuntimeException
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import com.bluetriangle.analytics.Tracker
-import com.bluetriangle.analytics.okhttp.BlueTriangleOkHttpInterceptor
 import com.bluetriangle.android.demo.R
 import com.bluetriangle.android.demo.groupingpoc.QuoteRequestHelper
-import com.google.android.material.card.MaterialCardView
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.suspendCancellableCoroutine
-import kotlinx.coroutines.withContext
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.OkHttp
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
-import org.json.JSONObject
-import java.io.IOException
-import kotlin.coroutines.resumeWithException
 
 class FourthTabFragment : Fragment() {
 
@@ -43,5 +25,9 @@ class FourthTabFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         QuoteRequestHelper.instance.setupQuoteUI(lifecycleScope, view)
+
+        view.findViewById<Button>(R.id.crash_button).setOnClickListener {
+            throw AndroidRuntimeException("Manual Crash")
+        }
     }
 }
