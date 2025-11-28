11bc54c91edb65c320f705390c278af95ae0d985
Ismail <ismails@alohatechnologydev.com>
2025-11-28 16:45:24 +0530

    Added synchronized blocks for networkMap

--- analytics/src/main/java/com/bluetriangle/analytics/networkstate/NetworkChangeListener.kt
+++ analytics/src/main/java/com/bluetriangle/analytics/networkstate/NetworkChangeListener.kt
@@ -8,7 +8,6 @@ import android.os.Build.VERSION_CODES.LOLLIPOP
 import androidx.annotation.RequiresApi
 import com.bluetriangle.analytics.Logger
 import com.bluetriangle.analytics.utility.value
-import java.util.concurrent.ConcurrentHashMap
 
 @RequiresApi(LOLLIPOP)
 @SuppressLint("MissingPermission")
@@ -17,12 +16,14 @@ internal class NetworkChangeListener(
     private val handlers: Array<NetworkTransportHandler>
 ) : NetworkCallback() {
 
-    private val networkMap = ConcurrentHashMap<Network, NetworkCapabilities?>()
+    private val networkMap = mutableMapOf<Network, NetworkCapabilities?>()
 
     override fun onAvailable(network: Network) {
         super.onAvailable(network)
         logger?.debug("NetworkMonitor: onAvailable")
-        networkMap.put(network, null)
+        synchronized(networkMap) {
+            networkMap.put(network, null)
+        }
     }
 
     private fun refreshNetworkState() {
@@ -36,35 +37,39 @@ internal class NetworkChangeListener(
         handlerMap: MutableMap<NetworkTransportHandler, Boolean>,
         currentNetworkState: Map<Network, NetworkCapabilities?>
     ) {
-        for(network in currentNetworkState) {
-            network.value?.let { capabilities ->
-                for(handler in handlers) {
-                    if(handler.isNetworkOnline(capabilities)) {
-                        handlerMap[handler] = true
-                    }
+        for ((_, capabilities) in currentNetworkState) {
+            if (capabilities == null) continue
+
+            for (handler in handlers) {
+                if (handler.isNetworkOnline(capabilities)) {
+                    handlerMap[handler] = true
                 }
             }
         }
     }
 
     private fun applyHandlerOnlineStatus(handlerMap: MutableMap<NetworkTransportHandler, Boolean>) {
-        for(handler in handlerMap) {
+        for (handler in handlerMap) {
             handler.key.setOnline(handler.value)
-            logger?.debug("NetworkMonitor: ${handler.key.state.value} - ${if(handler.value) "Online" else "Offline"}")
+            logger?.debug("NetworkMonitor: ${handler.key.state.value} - ${if (handler.value) "Online" else "Offline"}")
         }
     }
 
     override fun onLost(network: Network) {
         super.onLost(network)
         logger?.debug("NetworkMonitor: onLost")
-        networkMap.remove(network)
+        synchronized(networkMap) {
+            networkMap.remove(network)
+        }
         refreshNetworkState()
     }
 
     override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
         super.onCapabilitiesChanged(network, networkCapabilities)
         logger?.debug("NetworkMonitor: Capabilities Changed")
-        networkMap[network] = networkCapabilities
+        synchronized(networkMap) {
+            networkMap[network] = networkCapabilities
+        }
         refreshNetworkState()
     }
 
